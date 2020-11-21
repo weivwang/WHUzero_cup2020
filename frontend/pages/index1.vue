1bc406b63593688fabdff2100e0ecb9b4d32b7c0
Hooo1941 <Hooo1941@users.noreply.github.com>
2020-11-21 23:52:17 +0800

    vr,backend

--- frontend/pages/index1.vue
+++ frontend/pages/index1.vue
@@ -1,28 +1,11 @@
 <!--
  * @Date: 2020-11-21 21:56:16
  * @LastEditors: QiuJhao
- * @LastEditTime: 2020-11-21 21:58:00
+ * @LastEditTime: 2020-11-21 22:55:04
 -->
 <template>
-<div>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
-  <h1>index.</h1>
+<div id="box">
+	<iframe src="https://720yun.com/t/1e4je0yyuO5?scene_id=16517069" ></iframe>
   </div>
 </template>
 
@@ -33,5 +16,8 @@ export default {
 </script>
 
 <style>
-
+.box
+{
+	height: 100%;
+}
 </style>
--- preparework/index_by_hoo/index.vue
+++
@@ -1,8 +0,0 @@
-<!--
- * @Date: 2020-11-21 20:42:15
- * @LastEditors: QiuJhao
- * @LastEditTime: 2020-11-21 20:52:01
--->
-<template>
-<iframe src="./index.html"></iframe>
-</template>
