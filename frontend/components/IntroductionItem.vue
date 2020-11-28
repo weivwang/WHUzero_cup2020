62208e01fa007931c40add6bef206c19f2b845a2
orangeboyChen <chenenhan@qq.com>
2020-11-28 17:20:32 +0800

    update:组件复用化

--- frontend/components/IntroductionItem.vue
+++ frontend/components/IntroductionItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="introduction-item-root">
     <div class="float-image-div">
-      <img :src="require('~/static/' + imageFileName)" id="item-image" ref="administrationBuildingImage"
+      <img :src="require('~/static/' + imageFileName)" :id="'item-image-' + this.itemId" ref="administrationBuildingImage"
            @click="onItemClick"
            @mouseenter="administrationBuildingImage.isShowShadow = true"
            @mouseleave="onItemMouseLeave"
@@ -11,25 +11,23 @@
            }"/>
     </div>
 
-    <div id="card-box">
+    <div :id="'card-box-' + this.itemId">
       <el-card class="box-card">
         <div slot="header" class="clearfix">
-          <span style="font-size: 2rem">行政楼</span>
+          <span style="font-size: 2rem">{{cardTitle}}</span>
         </div>
 
         <el-row>
           <el-col :span="16">
-            <p>
-              武汉大学（Wuhan University）简称“武大”，是中华人民共和国教育部直属的综合性全国重点大学；位列“世界一流大学和一流学科”、“985工程”、“211工程”；入选学位授权自主审核单位、“珠峰计划”、“强基计划”、“2011计划”、“111计划”、卓越工程师教育培养计划、卓越法律人才教育培养计划、卓越医生教育培养计划、国家建设高水平大学公派研究生项目、国家级新工科研究与实践项目、一流网络安全学院建设示范项目高校、中国政府奖学金来华留学生接收院校、全国深化创新创业教育改革示范高校、国家大学生文化素质教育基地、大众创业万众创新示范基地、基础学科拔尖学生培养计划2.0基地，为欧亚-太平洋大学联盟、大学通识教育联盟、中国高校行星科学联盟、法学教育创新联盟、医学“双一流”建设联盟成员。
-            </p>
+            <p>{{cardContent}}</p>
           </el-col>
 
           <el-col :span="6" offset="2">
-            <el-image :src="require('~/static/WHUDefault.jpg')" class="card-image"></el-image>
+            <el-image :src="require('~/static/' + cardImageFileName)" class="card-image"></el-image>
           </el-col>
         </el-row>
         <el-divider></el-divider>
-        <div id="review-box">
+        <div class="review-box">
           <div class="el-card review-bubble is-always-shadow">
             <div class="el-card__body review-bubble-content">
               <span>好好好</span>
@@ -43,7 +41,7 @@
           </div>
         </div>
 
-        <div id="new-review-box">
+        <div class="new-review-box">
           <!--          <div class="el-card review-bubble is-always-shadow">-->
           <!--            <div class="el-card__body review-bubble-content">-->
           <!--            </div>-->
@@ -69,7 +67,7 @@ import $ from "jquery";
 
 export default {
   name: "IntroductionItem",
-  props: ['fromX', 'fromY', 'toX', 'toY', 'itemId', 'imageFileName', 'aniTime'],
+  props: ['fromX', 'fromY', 'toX', 'toY', 'itemId', 'imageFileName', 'aniTime', 'cardTitle', 'cardContent', 'cardImageFileName', 'width', 'height'],
   data() {
     return {
       administrationBuildingImage: {
@@ -196,9 +194,43 @@ export default {
   background: black;
   opacity: 0;
 
-      }`
-    )
+      }`, 0)
 
+    sheet.insertRule(
+      `#card-box-${this.itemId}{
+  position: relative;
+  float: right;
+
+  vertical-align: middle;
+
+  /*margin-right: 10%;*/
+
+  opacity: 0;
+  margin-right: -50%;
+
+
+  width: 40%;
+
+
+  z-index: 9999;
+
+  height: 100%;
+
+  display: flex;
+  align-items:center;
+  justify-content:center;
+
+      }`, 0);
+
+
+    sheet.insertRule(
+      `#item-image-${this.itemId}{
+  width: ${this.width}%;
+  height: ${this.height}%;
+  margin-left: ${this.fromX}%;
+  margin-top: ${this.fromY}%;
+  object-fit: fill;
+      }`, 0)
 
   },
   methods: {
@@ -218,8 +250,8 @@ export default {
 
 
       if(this.administrationBuildingImage.isShow){
-        $('#item-image').removeClass('animation-item-state-A-' + this.itemId).addClass('animation-item-state-B-' + this.itemId);
-        $('#card-box').removeClass('animation-card-state-A-' + this.itemId).addClass('animation-card-state-B-' + this.itemId);
+        $('#item-image-' + this.itemId).removeClass('animation-item-state-A-' + this.itemId).addClass('animation-item-state-B-' + this.itemId);
+        $('#card-box-' + this.itemId).removeClass('animation-card-state-A-' + this.itemId).addClass('animation-card-state-B-' + this.itemId);
         $('#modal-' + this.itemId).removeClass('animation-modal-state-A-' + this.itemId).addClass('animation-modal-state-B-' + this.itemId);
 
         clearInterval(this.administrationBuildingImage.clearInterval);
@@ -231,8 +263,8 @@ export default {
 
       }
       else{
-        $('#item-image').removeClass('animation-item-state-B-' + this.itemId).addClass('animation-item-state-A-' + this.itemId);
-        $('#card-box').removeClass('animation-card-state-B-' + this.itemId).addClass('animation-card-state-A-' + this.itemId);
+        $('#item-image-' + this.itemId).removeClass('animation-item-state-B-' + this.itemId).addClass('animation-item-state-A-' + this.itemId);
+        $('#card-box-' + this.itemId).removeClass('animation-card-state-B-' + this.itemId).addClass('animation-card-state-A-' + this.itemId);
         $('#modal-' + this.itemId).removeClass('animation-modal-state-B-' + this.itemId).addClass('animation-modal-state-A-' + this.itemId);
 
         clearInterval(this.administrationBuildingImage.clearInterval);
@@ -256,82 +288,23 @@ export default {
 }
 
 
-
-
-
-#card-box{
-  position: relative;
-  float: right;
-
-  vertical-align: middle;
-
-  /*margin-right: 10%;*/
-
-  opacity: 0;
-  margin-right: -50%;
-
-
-  width: 40%;
-
-
-  z-index: 9999;
-
-  height: 100%;
-
-  display: flex;
-  align-items:center;
-  justify-content:center;
-
-
-}
-
 .box-card {
   display: table-cell;
   vertical-align: center;
 }
 
-
-
-
-
-
-.background-image, #item-image {
-  width: 100%;
-  height: 100%;
-  object-fit: fill;
-}
-
-.single-slide-div, .float-image-div {
-  width: 100%;
-  height: 100%;
-
-}
-
-.background-image {
-  position: absolute;
-}
-
-
-
-#item-image {
-  width: 38%;
-  height: 49%;
-
-  margin-left: 30%;
-  margin-top: 4.5%;
-
-}
-
 .float-image-div{
   position: absolute;
   z-index: 999;
+  width: 100%;
+  height: 100%;
 }
 
 .card-image{
 
 }
 
-#review-box {
+.review-box {
   display: flex;
   flex-wrap: wrap;
 
@@ -353,7 +326,7 @@ export default {
 
 }
 
-#new-review-box {
+.new-review-box {
   width: 100%;
 }
 
@@ -361,4 +334,5 @@ export default {
   display: flex;
   flex-wrap: wrap
 }
+
 </style>
--- frontend/components/IntroductionSlide.vue
+++ frontend/components/IntroductionSlide.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="single-slide-div">
     <img class="background-image" :src="require('../static/' + backgroundImageFileName)"/>
-    <slot name="item"></slot>
+    <slot name="items"></slot>
   </div>
 </template>
 
@@ -28,10 +28,6 @@ export default {
     },
 
     onItemClick: function (){
-
-      let item = this.$refs.administrationBuildingImage;
-
-
       if(this.administrationBuildingImage.isShow){
         $('#administration-building-image').removeClass('animation-item-state-A').addClass('animation-item-state-B');
         $('#card-box').removeClass('animation-card-state-A').addClass('animation-card-state-B');
@@ -41,9 +37,6 @@ export default {
         this.administrationBuildingImage.clearInterval = setInterval(() => {
           this.administrationBuildingImage.isShow = false;
         }, 800);
-
-
-
       }
       else{
         $('#administration-building-image').removeClass('animation-item-state-B').addClass('animation-item-state-A');
@@ -52,10 +45,7 @@ export default {
 
         clearInterval(this.administrationBuildingImage.clearInterval);
         this.administrationBuildingImage.isShow = true;
-
       }
-
-
     },
   },
   data(){
@@ -84,34 +74,6 @@ export default {
         clearInterval: null
       }
     }
-  },
-  mounted() {
-    this.administrationBuildingImage.marginTop = window.innerHeight * this.administrationBuildingImage.setMarginTop;
-
-    window.onresize = () => {
-      return (() => {
-        let height = window.innerHeight;
-        this.administrationBuildingImage.marginTop = height * this.administrationBuildingImage.setMarginTop;
-
-        console.log(height * this.administrationBuildingImage.marginTop);
-
-      })()
-    }
-
-    // let style = document.createElement('style');
-    // style.setAttribute('type', 'text/css');
-    // document.head.appendChild(style);
-    // let sheet = style.sheet;
-    // sheet.insertRule(
-    //   `@keyframes pointermove` +(index + 1) +`{
-  	// 		from {
-		// 	    transform: rotate(300deg);
-  	// 		}
- 		// 	 to {
- 		// 	 //注意：动态的计算需要先计算再赋值，不然浏览器会直接当字符串来解析
-   	// 			 transform: rotate(` +(300 + element.pointerEnd) +`deg);}
-		// 	}`,0
-    // );
   }
 }
 </script>
@@ -133,9 +95,5 @@ export default {
 .background-image {
   position: absolute;
 }
-
-
-
-
 
 </style>
